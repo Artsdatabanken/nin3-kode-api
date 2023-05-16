e878d28284a3ef30332d435768d94a060f2eff82
Stein A. Hoem <stein.hoem@gmail.com>
2023-05-16 14:14:53 +0200

    HELLO WORLD

--- NiN3KodeAPI/Program.cs
+++ NiN3KodeAPI/Program.cs
@@ -7,61 +7,61 @@ using System.Collections;
 using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-if (builder.Environment.IsProduction() || builder.Environment.IsStaging())
-{
-    builder.Configuration.AddAzureKeyVault(
-        new Uri($"https://{builder.Configuration["KeyVault:Name"]}.vault.azure.net/"),
-        new DefaultAzureCredential());
-}
+//if (builder.Environment.IsProduction() || builder.Environment.IsStaging())
+//{
+//    builder.Configuration.AddAzureKeyVault(
+//        new Uri($"https://{builder.Configuration["KeyVault:Name"]}.vault.azure.net/"),
+//        new DefaultAzureCredential());
+//}
 
 
-var controllers_to_exclude_from_prod = new ArrayList() {"WeatherForecast", "Admin"};
+//var controllers_to_exclude_from_prod = new ArrayList() {"WeatherForecast", "Admin"};
 
-// Add services to the container.
-//var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-builder.Services.AddControllers(o =>
-{
-    //if (environment == "Development")
-    if (builder.Environment.IsProduction())
-    {
-        o.Conventions.Add(new ActionHidingConvention(controllers_to_exclude_from_prod));
-    }
-});
-builder.Services.AddDbContext<NiN3DbContext>(options =>
-{
-    if (builder.Configuration.GetConnectionString("default").Contains("Server=tcp"))
-    {
-         options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
-    }
-    else
-    {
-        options.UseSqlite(builder.Configuration.GetConnectionString("default"));
-    }
+//// Add services to the container.
+////var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+//builder.Services.AddControllers(o =>
+//{
+//    //if (environment == "Development")
+//    if (builder.Environment.IsProduction())
+//    {
+//        o.Conventions.Add(new ActionHidingConvention(controllers_to_exclude_from_prod));
+//    }
+//});
+//builder.Services.AddDbContext<NiN3DbContext>(options =>
+//{
+//    if (builder.Configuration.GetConnectionString("default").Contains("Server=tcp"))
+//    {
+//         options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
+//    }
+//    else
+//    {
+//        options.UseSqlite(builder.Configuration.GetConnectionString("default"));
+//    }
    
-    //options.UseSqlite(builder.Configuration.GetConnectionString("default"));
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddScoped<IAdminService, AdminService>();
-builder.Services.AddSingleton<ISService, SService>();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//    //options.UseSqlite(builder.Configuration.GetConnectionString("default"));
+//});
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddScoped<IAdminService, AdminService>();
+//builder.Services.AddSingleton<ISService, SService>();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    var scope = app.Services.CreateScope();
-    var db = scope.ServiceProvider.GetService<NiN3DbContext>();
-    //if (db != null) { db.Database.Migrate(); };
-}
+//if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//    var scope = app.Services.CreateScope();
+//    var db = scope.ServiceProvider.GetService<NiN3DbContext>();
+//    //if (db != null) { db.Database.Migrate(); };
+//}
 
 //app.UseHttpsRedirection();
 
 //app.UseAuthorization();
 app.MapGet("/", () => "Hello World!");
-app.MapControllers();
+//app.MapControllers();
 ////* One way to get key vault secrets */
 //var SService = app.Services.GetRequiredService<ISService>();
 ////SService.Startup();
