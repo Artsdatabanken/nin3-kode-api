00f225452c710d7179f6dc17d68c515c6ab7b7cd
ADB\sto <stig.thorseth@artdatabanken.no>
2023-11-29 10:15:04 +0100

    db-info tabell:timestamp for db-oppretting

--- NiN3.Infrastructure/Services/LoaderService.cs
+++ NiN3.Infrastructure/Services/LoaderService.cs
@@ -155,6 +155,11 @@ namespace NiN.Infrastructure.Services
             CreateHovedtypeVariabeltrinnView();
             CreateSjekkUnikeHovedklasserView();
             CreateHovedtypeKleView();
+            CreateAlleLangkoderView();
+            CreateDuplikateLangkoderView();
+            CreateDBInfoTable();
+            SetBuildTimeInDb_infoTable();
+
             WriteToFile($"END: loading is FINISHED :) (Log is written to {logpath})");
         }
 
@@ -1521,6 +1526,47 @@ namespace NiN.Infrastructure.Services
                         Where KartleggingsenhetId = K.Id AND HovedtypeId = H.Id
                              and E.Ordinal = K.Maalestokk and E.Enumtype = 'MaalestokkEnum'
                              Order by HTKode, K.Maalestokk";
+            _context.Database.ExecuteSqlRaw(sql);
+        }
+
+        public void CreateAlleLangkoderView() {
+            var sql = @"Create view AlleLangkoderView AS
+                        select 'Type' as Klasse, Kode, Langkode from Type
+                        UNION ALL
+                        select 'Hovedtypegruppe' as Klasse, Kode, Langkode from Hovedtypegruppe
+                        UNION ALL
+                        select 'Hovedtype' as Klasse, Kode, Langkode from Hovedtype
+                        UNION ALL
+                        select 'Grunntype' as Klasse, Kode, Langkode from Grunntype
+                        UNION ALL
+                        select 'Kartleggingsenhet' as Klasse, Kode, Langkode from Kartleggingsenhet
+                        UNION ALL
+                        select 'Variabel' as Klasse, Kode, Langkode from Variabel
+                        UNION ALL
+                        select 'Variabelnavn' as Klasse, Kode, Langkode from Variabelnavn";
+            _context.Database.ExecuteSqlRaw(sql);
+        }
+
+        public void CreateDuplikateLangkoderView() {
+            var sql = @"create view DuplikateLangkoderView AS
+                        SELECT Langkode, Klasse, COUNT(*) AS DuplicateCount
+                        FROM AlleLangkoderView
+                        GROUP BY Langkode, Klasse
+                        HAVING COUNT(*) > 1";
+            _context.Database.ExecuteSqlRaw(sql);
+        }
+
+        public void CreateDBInfoTable() { 
+            var sql = @"CREATE TABLE db_info (
+                            Tittel TEXT,
+                            Verdi TEXT
+                        )";
+            _context.Database.ExecuteSqlRaw(sql);
+        }
+
+        public void SetBuildTimeInDb_infoTable()
+        {
+            var sql = @"INSERT INTO db_info (Tittel, Verdi) VALUES ('Opprettet', strftime('%Y%m%d_%H%M%S', 'now', 'localtime'))";
             _context.Database.ExecuteSqlRaw(sql);
         }
 
